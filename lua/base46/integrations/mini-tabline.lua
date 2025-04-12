db31f9b201f41c1e00672ec1895d9bdf6413a714
yuukibarns <yuukibarns@gmail.com>
2025-04-13 00:23:50 +0800

    feat(integration): mini-tabline

---
+++ lua/base46/integrations/mini-tabline.lua
@@ -0,0 +1,57 @@
+local colors = require("base46").get_theme_tb "base_30"
+
+local config = require "nvconfig"
+
+local highlights = {
+  MiniTablineCurrent = {
+    fg = colors.white,
+    bg = colors.black,
+  },
+
+  MiniTablineVisible = {
+    fg = colors.white,
+    bg = colors.black,
+  },
+
+  MiniTablineHidden = {
+    fg = colors.light_grey,
+    bg = colors.black2,
+  },
+
+  MiniTablineModifiedCurrent = {
+    fg = colors.green,
+    bg = colors.black,
+  },
+
+  MiniTablineModifiedVisible = {
+    fg = colors.green,
+    bg = colors.black,
+  },
+
+  MiniTablineModifiedHidden = {
+    fg = colors.red,
+    bg = colors.black2,
+  },
+
+  MiniTablineTabpagesection = {
+    fg = colors.black,
+    bg = colors.blue,
+  },
+}
+
+local hlgroups_glassy = {
+  "MiniTablineCurrent",
+  "MiniTablineVisible",
+  "MiniTablineHidden",
+  "MiniTablineModifiedCurrent",
+  "MiniTablineModifiedVisible",
+  "MiniTablineModifiedHidden",
+}
+
+if config.base46.transparency then
+  for _, val in ipairs(hlgroups_glassy) do
+    highlights[val].bg = "NONE"
+  end
+end
+
+return highlights
