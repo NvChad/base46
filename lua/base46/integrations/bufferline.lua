1266144a54265ef0242c8243788212e157b854d4
dodofarm <ea01052002@gmail.com>
2025-07-20 18:25:10 +0100

    Small fix to make arrows on sides of bufferline blend in better

--- lua/base46/integrations/bufferline.lua
+++ lua/base46/integrations/bufferline.lua
@@ -1,6 +1,10 @@
 local colors = require("base46").get_theme_tb "base_30"
 
 return {
+  BufferLineTruncMarker = {
+    fg = colors.light_grey,
+    bg = colors.black2,
+  },
 
   BufferLineBackground = {
     fg = colors.light_grey,
